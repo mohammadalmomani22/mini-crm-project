939fdcda045df2d6c5dcf9037ff47629173bb5c9
mohammadalmomani22 <moemomani99@gmail.com>
2026-02-28 00:55:43 +0300

    Add Docker setup with PostgreSQL and healthcheck

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -59,8 +59,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -5,13 +5,17 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mini_crm',
+        'USER': 'mini_crm_user',
+        'PASSWORD': '123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
